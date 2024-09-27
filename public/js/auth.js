3bfb995a7bb7345aee1e64d715f353a667fe39f8
Goubigon <goubigon@gmail.com>
2024-09-27 10:29:04 +0200

    Login functions extracted to auth.js

---
+++ public/js/auth.js
@@ -0,0 +1,99 @@
+//Check data base
+export async function logUser(email, password) {
+    try {
+        const response = await fetch('/user-data/logUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
+
+        //contains message, access token, refresh token
+        const result = await response.json();
+        if (response.ok) {
+            console.log(result.message);
+            return result.accessToken;
+        } else {
+            document.getElementById('errorMessage').innerHTML = result.message;
+        }
+
+    } catch (error) {
+        console.error('Error logging in:', error);
+    }
+}
+
+
+
+export async function getNewToken() {
+    try {
+        const response = await fetch('/user-data/token', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            console.log("Token Refreshed");
+            console.log(result.message)
+            console.log("new generated access token : " + result.accessToken)
+            return result.accessToken;
+        } else {
+            document.getElementById('errorMessage').innerHTML = result.message;
+        }
+
+    } catch (error) {
+        console.error('Error logging in:', error);
+    }
+}
+
+export async function getUserInfo(authToken) {
+    try {
+        const response = await fetch(`/user-data/getUser`, {
+            method: 'GET',
+
+            headers: {
+                'Content-Type': "application/json",
+                'Authorization': `Bearer ${authToken}`
+            },
+
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            console.log(result);
+        } else {
+            console.error('Failed to retrieve answers.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+export async function logoutUser(authToken) {
+    try {
+        const response = await fetch(`/user-data/logout`, {
+            method: 'DELETE',
+
+            headers: {
+                'Content-Type': "application/json",
+                'Authorization': `Bearer ${authToken}`
+            },
+
+        });
+
+        if (response.ok) {
+            console.log('Logout successful, cookie cleared.');
+        } else {
+            const errorData = await response.json();
+            console.error('Error during logout:', errorData.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
--- public/js/login-script.js
+++ public/js/login-script.js
@@ -1,117 +1,24 @@
 
 
+
+import { logUser, getNewToken, getUserInfo, logoutUser } from './auth.js';
+
 let authToken;
-//Check data base
-async function logUser(email, password) {
-    try {
-        const response = await fetch('/user-data/logUser', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password
-            })
-        });
-
-        //contains message, access token, refresh token
-        const result = await response.json();
-        if (response.ok) {
-            console.log(result.message);
-            authToken = result.accessToken
-            console.log("auth : " + authToken);
-
-            refreshToken = result.refreshToken
-            console.log("refresh : " + refreshToken);
-        } else {
-            document.getElementById('errorMessage').innerHTML = result.message;
-        }
-
-    } catch (error) {
-        console.error('Error logging in:', error);
-    }
-}
-
-
-async function getNewToken() {
-    try {
-        const response = await fetch('/user-data/token', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        });
-
-        const result = await response.json();
-        if (response.ok) {
-            console.log("Token Refreshed");
-            console.log(result.message)
-            console.log("new generated access token : "+ result.accessToken)
-            authToken = result.accessToken;
-        } else {
-            document.getElementById('errorMessage').innerHTML = result.message;
-        }
-
-    } catch (error) {
-        console.error('Error logging in:', error);
-    }
-}
-
-async function getUserInfo(authToken) {
-    try {
-        const response = await fetch(`/user-data/getUser`, {
-            method: 'GET',
-            
-            headers: {
-                'Content-Type': "application/json",
-                'Authorization': `Bearer ${authToken}`
-            },
-            
-        });
-
-        if (response.ok) {
-            const result = await response.json();
-            console.log(result); 
-        } else {
-            console.error('Failed to retrieve answers.');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-async function logoutUser(authToken) {
-    try {
-        const response = await fetch(`/user-data/logout`, {
-            method: 'DELETE',
-            
-            headers: {
-                'Content-Type': "application/json",
-                'Authorization': `Bearer ${authToken}`
-            },
-            
-        });
-
-        if (response.ok) {
-            console.log('Logout successful, cookie cleared.');
-        } else {
-            const errorData = await response.json();
-            console.error('Error during logout:', errorData.message);
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
 
 document.getElementById('loginForm').addEventListener('submit', async (event) => {
     event.preventDefault();
 
+    console.log("Pressed Login Button")
+
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const response = await logUser(email, password);
+    authToken = await logUser(email, password);
+    console.log("Auth Token : " + authToken)
+    
+    if (authToken) {
+        //window.location.href = 'home';
+    }
 })
 
 document.getElementById('refreshTokenButton').addEventListener('click', async (event) => {
