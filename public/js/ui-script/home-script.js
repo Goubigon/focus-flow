bd3a346934eefed96841e4f6e28a9f9a77f25909
Goubigon <goubigon@gmail.com>
2024-10-14 16:50:39 +0200

    User's name is displayed on home page

--- public/js/ui-script/home-script.js
+++ public/js/ui-script/home-script.js
@@ -2,8 +2,8 @@ import { getUserInfo, keepAuthenticate } from '../client-api/auth_api.js';
 
 
 window.onload = async function () {
-    // const userJson = await getUserInfo()
-    // document.getElementById('user-name').textContent = userJson.mUsername;
+    const userJson = await getUserInfo()
+    document.getElementById('user-name').textContent = userJson.mUsername;
 
     await keepAuthenticate()
 };
