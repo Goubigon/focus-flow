4cd287cbae9f6e445de9f512a017dd097ecac131
Goubigon <goubigon@gmail.com>
2024-10-15 16:27:42 +0200

    Removed logbar script + better style

--- public/js/ui-script/logbar-script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("loginButton").addEventListener('click', async () =>{
-    console.log("pressed login")
-    const response = await logoutUser()
-    console.log(response ? "success logout" : "fail logout");
-})
-
-window.onload = () => {
-    console.log("Load navbar logout script ");
-}
