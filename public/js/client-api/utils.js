70c7f74626d5634b14d3a6f93a5453a3bf343b61
Goubigon <goubigon@gmail.com>
2024-10-23 16:49:13 +0200

    update utils getCleanDate(pDate)

--- public/js/client-api/utils.js
+++ public/js/client-api/utils.js
@@ -1,12 +1,10 @@
-export function getCurrentDateTime() {
-    const now = new Date();
-
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0');
-    const day = String(now.getDate()).padStart(2, '0');
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
+export function getCleanDateTime(pDate) {
+    const year = pDate.getFullYear();
+    const month = String(pDate.getMonth() + 1).padStart(2, '0');
+    const day = String(pDate.getDate()).padStart(2, '0');
+    const hours = String(pDate.getHours()).padStart(2, '0');
+    const minutes = String(pDate.getMinutes()).padStart(2, '0');
+    const seconds = String(pDate.getSeconds()).padStart(2, '0');
 
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 };
