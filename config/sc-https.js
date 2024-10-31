e5b4fe832051d588ec63e91668c4ddf3c84a3124
Goubigon <goubigon@gmail.com>
2024-10-31 15:55:46 +0100

    Reverting http changes

---
+++ config/sc-https.js
@@ -0,0 +1,22 @@
+//Handle https server
+//looking for files in ../certificates/localhost*.pem
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const port = 8000;
+
+function startServer(app) {
+    //HTTPS certificates
+    const options = {
+        key: fs.readFileSync(path.join(__dirname, '../certificates/localhost-key.pem')),
+        cert: fs.readFileSync(path.join(__dirname, '../certificates/localhost.pem'))
+    };
+
+    const server = https.createServer(options, app);
+    server.listen(port, () => {
+        console.log(`Server running at https://localhost:${port}/`);
+    })
+}
+
+module.exports = { startServer };
