5872134d0ecbd9be0e66159292a60d52d937c047
Goubigon <goubigon@gmail.com>
2024-11-04 14:25:28 +0100

    Updating clean guest data

--- db_installation/math_session_parameters_seed.sql
+++ db_installation/math_session_parameters_seed.sql
@@ -3,4 +3,5 @@ INSERT INTO math_db.math_session_parameters (mMinNumber,mMaxNumber,mFloatNumber,
 	 (0,10,0,2,0,1,0,20),
 	 (0,10,0,2,0,0,1,20),
 	 (0,20,0,2,1,1,0,10),
-	 (0,20,0,2,1,1,1,5);
+	 (0,20,0,2,1,1,1,5),
+	 (10,20,0,2,1,0,0,10);
--- db_installation/math_user_stat_seed.sql
+++ db_installation/math_user_stat_seed.sql
@@ -1,2 +1,2 @@
 INSERT INTO math_db.math_user_stat (mUserIdentifier,mLogNumber,mSessionCount,mLastSessionDate,mTotalSessionTime) VALUES
-	 (1,0,0,NULL,0);
+	 (1,2,12,'2024-11-04 14:19:13',324.48);
