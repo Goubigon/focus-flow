5308c27a5830c41f8291648fb16bb6168b4917ff
Goubigon <goubigon@gmail.com>
2024-11-05 12:46:15 +0100

    Proofing database, tables or data empty

--- config/database/sc-init-db.js
+++ config/database/sc-init-db.js
@@ -35,35 +35,42 @@ async function seedingDatabase(connection) {
 
 async function initializeDatabase() {
   let connection;
-
   try {
-    
-    // Check if the database exists
     connection = await pool.getConnection();
-    const [databases] = await connection.query('SHOW DATABASES LIKE ?', [process.env.DB_NAME_MATH]);
 
-    if (databases.length === 0) {
-      console.log(`Database ${process.env.DB_NAME_MATH} does not exist. Creating and seeding...`);
+    const [math_db] = await connection.query('SHOW DATABASES LIKE ?', [process.env.DB_NAME_MATH]);
+    if (math_db.length > 0) {
+      console.log(`Database ${process.env.DB_NAME_MATH} exists.`);
+      await connection.query(`USE ${process.env.DB_NAME_MATH}`);
+
+      const [user_cred_table] = await connection.query('SHOW TABLES LIKE ?', ['math_user_credential']);
+      if (user_cred_table.length > 0) {
+        console.log(`Table math_user_credential exists.`);
+
+        const [user_cred_rows] = await connection.query("SELECT COUNT(*) AS count FROM math_user_credential");
+        const [user_answer_rows] = await connection.query('SELECT COUNT(*) AS count FROM math_answer');
+
+        if (user_cred_rows[0].count > 0 && user_answer_rows[0].count > 200) {
+          console.log(`Database ${process.env.DB_NAME_MATH} already has guest data. Skipping seeding.`);
+        }
+        else { // no data
+          console.log(`Table math_user_credential has no content, creating guest data...`);
+          await seedingDatabase(connection);
+        }
+
+      }
+      else { // no tables
+        console.log(`Database ${process.env.DB_NAME_MATH} doesn't have tables. Creating and seeding...`);
+        await executeSQLFile(connection, path.join(__dirname, '../../db_installation/1_math_db_schema.sql'));
+        await seedingDatabase(connection);
+        console.log(`Tables created successfully.`);
+      }
+    }
+    else { // no database
+      console.log(`Database ${process.env.DB_NAME_MATH} doesn't exist. Creating and seeding...`);
       await executeSQLFile(connection, path.join(__dirname, '../../db_installation/1_math_db_schema.sql'));
       await seedingDatabase(connection);
       console.log(`Database ${process.env.DB_NAME_MATH} created and seeded successfully.`);
-    }
-
-    else {
-      console.log(`Database ${process.env.DB_NAME_MATH} already exists. Skipping creation.`);
-
-      await connection.query(`USE ${process.env.DB_NAME_MATH}`);
-      const [user_cred_table] = await connection.query("SELECT COUNT(*) AS count FROM math_user_credential");
-      const [user_answer_table] = await connection.query('SELECT COUNT(*) AS count FROM math_answer');
-      
-      if (user_cred_table[0].count === 0 || user_answer_table[0].count < 200) {
-        console.log(`Database ${process.env.DB_NAME_MATH} has no content, creating guest data...`);
-        await seedingDatabase(connection);
-      }
-      else{
-        console.log(`Database ${process.env.DB_NAME_MATH} already has guest data. Skipping seeding.`);
-      }
-
     }
   } catch (error) {
     console.error('Database initialization failed:', error);
@@ -72,4 +79,4 @@ async function initializeDatabase() {
   }
 }
 
-module.exports = initializeDatabase;
+module.exports = { initializeDatabase };
