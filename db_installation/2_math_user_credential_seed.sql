00727fd3ad8fd0c17a402178cb99d2ed62e80cf2
Goubigon <goubigon@gmail.com>
2024-11-25 17:45:30 +0100

    init db restored

---
+++ db_installation/2_math_user_credential_seed.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO math_db.math_user_credential (mUserIdentifier, mUsername,mEmail,mHashedPassword,mRole,mCreationDate) VALUES
+	 (1, 'Guest','guest@guest.guest','$2b$10$iYvr3OeX80f7VoGHSX/bG.4.Gva2mLHcq/M1PUihuQ07PcyjpDwKG','user','2024-10-10 16:04:31');
--- db_installation/math_user_stat_seed.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO math_db.math_user_stat (mUserIdentifier,mLogNumber,mSessionCount,mLastSessionDate,mTotalSessionTime) VALUES
-	 (1,32,87,'2024-10-26 17:29:56',1915.56);
